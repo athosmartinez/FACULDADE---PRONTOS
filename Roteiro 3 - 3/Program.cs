ea9c2d3f6f0adda1b88f023c12018ba5fef40bb5
Athos Martinez <athosandrade04@gmail.com>
2022-04-14 10:50:35 -0300

    Create Program.cs

---
+++ Roteiro 3 - 3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Roteiro3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1;
+            Console.WriteLine("Digite um valor:");
+            n1 = double.Parse(Console.ReadLine());
+            if (n1 % 2 == 0) {
+            Console.WriteLine("O número é par.");
+            }
+            else 
+            {
+                Console.WriteLine("O número é impar.");
+                
+            }
+            
+           Console.ReadKey();
+           Console.ReadLine();
+           
+
+
+            
+           
+
+            
+
+            
+
+
+
+        }
+    }
+}
