14f00ab8f7dbf898daa4df3ea30c0532c6fbd391
Athos Martinez <athosandrade04@gmail.com>
2022-04-14 11:04:16 -0300

    Create Program.cs

---
+++ Roteiro 2 - 3/Roteiro 3 - 2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Roteiro1diga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float preco, novovalor ;
+            Console.WriteLine("Entre o valor do produto:");
+            preco = float.Parse(Console.ReadLine());
+            novovalor = preco - (preco * 10/100);
+            Console.WriteLine("Ficou por: " +novovalor);
+            
+
+
+
+        }
+    }
+}
