576e31c6fa7512727cacd95952537e7657d14433
Athos Martinez <athosandrade04@gmail.com>
2022-04-16 19:47:39 -0300

    Roteiro 5

---
+++ Roteiro 5/Roteiro 5 - 4/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Roteiro3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          int n1, i = 1;
+          do
+          {
+              n1 =  int.Parse(Console.ReadLine());
+          } while (n1<0);
+          while (i<n1)
+          {
+              if (n1 % i == 0)
+              {
+                  Console.WriteLine(i);
+              }i++;
+          }
+
+            
+            
+            
+          
+        
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
