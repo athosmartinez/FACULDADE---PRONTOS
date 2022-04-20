39423501a15c592bd0100dbfde40810a2858900c
Athos Martinez <athosandrade04@gmail.com>
2022-04-20 08:30:10 -0300

    Roteiro 5

---
+++ Roteiro 5/Roteiro 5 - 9/dowhile.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=0, x=0, fibonacci=1;
+
+            do {
+                int y=x;
+                x= fibonacci;
+                fibonacci= x + y;
+                Console.WriteLine("{0}", fibonacci);
+                i++;
+            } while(i<8);
+        }
+    }
+}
---
+++ Roteiro 5/Roteiro 5 - 9/while.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program2
+    {
+        static void Main(string[] args)
+        {
+            int i=0, x=0, fibonacci=1;
+
+            while (i<8) {
+                int y=x;
+                x= fibonacci;
+                fibonacci= x + y;
+                Console.WriteLine("{0}", fibonacci);
+                i++;
+            } 
+        }
+    }
+}
