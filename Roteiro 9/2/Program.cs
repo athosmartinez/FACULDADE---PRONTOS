b58f2a34bdd44bbb3f5d49dca19d4fecc952e57d
Athos Martinez <athosandrade04@gmail.com>
2022-05-18 20:00:59 -0300

    Roteiro 9

--- Roteiro 9/2/Program.cs
+++ Roteiro 9/2/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args){
+            
+        }
+    
+
+        {
+            void LeVetor(float[] vetor)
+            {
+                for (int i = 0; i < vetor.Length; i++)
+                {
+                    Console.Write("Entre com a nota do aluno [{0}]", i + 1);
+                    vetor[i] = float.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
