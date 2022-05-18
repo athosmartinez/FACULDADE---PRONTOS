b171ddb81c568dfc02a8975f33dbbfe9ad1f729a
Athos Martinez <athosandrade04@gmail.com>
2022-05-18 09:45:40 -0300

    Roteiro 8

--- Roteiro 8/6/Program.cs
+++ Roteiro 8/6/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace ex6 // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int dias, dia = 1, mes = 1, ano = 1900;
+            Console.WriteLine("Digite o número de dias corridos:");
+            dias = int.Parse(Console.ReadLine());
+            WhatDate(ref dia, ref mes, ref ano, ref dias);
         }
+
+        static void WhatDate(ref int d, ref int m, ref int a, ref int ds)
+        {
+            a = (ds / 365);
+            int restoano = ds % 365;
+            m = restoano / 30;
+            int restomes = restoano % 30;
+            d = restomes;
+            Console.WriteLine(a);
+                if(ds > 0){
+                    a += 1900;
+                }
+                else {
+                    a = 1900 +a;
+                    m = 12 + m;
+                    d = 30 +d;
+                }
+                Console.WriteLine("dia {0} : Mês {1} : Ano {2}", d, m, a);
+        }
+
     }
 }
