fa70f8840584905eb05052c5fc11be908fb624d7
Athos Martinez <athosandrade04@gmail.com>
2022-05-18 20:55:34 -0300

    Roteiro 9

--- Roteiro 9/2/Program.cs
+++ Roteiro 9/2/Program.cs
@@ -1,23 +1,59 @@
 ﻿using System;
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace ex2 // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args){
-            
-        }
-    
-
+        static void Main(string[] args)
         {
-            void LeVetor(float[] vetor)
+            int quantidade, positivos = 0, negativos = 0;
+            Console.WriteLine("Digite a quantidade de números que irá digitar");
+            quantidade = int.Parse(Console.ReadLine());
+            float[] listaa = new float[quantidade];
+            LeVetor(listaa);
+            float[] listan = new float[quantidade];
+            float[] listap = new float[quantidade];
+            for (int i = 0; i < quantidade; i++)
             {
-                for (int i = 0; i < vetor.Length; i++)
+                Console.Write(" " + listaa[i]);
+            }
+
+            for (int i = 0; i < quantidade; i++)
+            {
+
+                if (listaa[i] < 0)
                 {
-                    Console.Write("Entre com a nota do aluno [{0}]", i + 1);
-                    vetor[i] = float.Parse(Console.ReadLine());
+                    listan[negativos] = listaa[i];
+
+                    negativos++;
+                }
+                else if (listaa[i] > 0)
+                {
+                    listap[positivos] = listaa[i];
+                    positivos++;
                 }
             }
+
+            Console.WriteLine("São {0} positivos eles são ", positivos);
+            Console.WriteLine("São {0} negativos eles são ", negativos);
+            for (int i = 0; i < positivos; i++)
+            {
+                Console.Write(" " + listap[i]);
+            }
+
+            for (int i = 0; i < negativos; i++)
+            {
+                Console.Write(" " + listan[i]);
+            }
         }
+        static void LeVetor(float[] vetor)
+        {
+            for (int i = 0; i < vetor.Length; i++)
+            {
+                Console.Write("", i + 1);
+                vetor[i] = float.Parse(Console.ReadLine());
+            }
+        }
+
     }
 }
