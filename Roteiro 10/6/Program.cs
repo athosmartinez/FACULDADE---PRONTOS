c5b0d1159eaa1a6c2b4234b4e07ffe03bf5bdcd1
Athos Martinez <athosandrade04@gmail.com>
2022-06-02 18:03:02 -0300

    Roteiro 10

--- Roteiro 10/6/Program.cs
+++ Roteiro 10/6/Program.cs
@@ -7,11 +7,11 @@ namespace MyApp // Note: actual namespace depends on the project name.
         static void Main(string[] args)
         {
 
-            int[,] MatrizD = new int[4, 2];
-            int[,] TransportDMatrizC = new int[2, 4];
+            int[,] MatrizD = new int[4, 4];
+            int[,] TransportDMatrizC = new int[4, 4];
             LeMatriz(MatrizD);
             Tranporte(ref MatrizD, ref TransportDMatrizC);
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
                 {
@@ -24,7 +24,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
         {
             for (int i = 0; i < 4; i++)
             {
-                for (int j = 0; j < 2; j++)
+                for (int j = 0; j < 4; j++)
                 {
                     Console.WriteLine("[{0},{1}]=", i + 1, j + 1);
                     Matriz[i, j] = int.Parse(Console.ReadLine());
@@ -33,9 +33,10 @@ namespace MyApp // Note: actual namespace depends on the project name.
         }
         static void Tranporte(ref int[,] D, ref int[,] C)
         {
-            for (int i = 0; i < 2; i++)
+            for (int i = 0; i < 4; i++)
                 for (int j = 0; j < 4; j++)
                     C[i, j] = D[j, i];
+                    
             for (int i = 0; i < 4; i++)
             {
                 for (int j = 0; j < 4; j++)
