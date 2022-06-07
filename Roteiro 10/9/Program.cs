073c62681af433b463edb083326d59494a001ed1
Athos Martinez <athosandrade04@gmail.com>
2022-06-07 16:23:35 -0300

    Roteiro 10

--- Roteiro 10/9/Program.cs
+++ Roteiro 10/9/Program.cs
@@ -1,2 +1,58 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] MatrizA = new int[4, 4];
+            LeMatriz(MatrizA);
+            ImprimeMatriz(MatrizA);
+            ImprimirDiagonal(MatrizA);
+
+        }
+        static void ImprimeMatriz(int[,] Imprimir)
+        {
+            Console.WriteLine("OLHA A MATRIZ AÍ OH:");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write(Imprimir[i, j] + " \t ");
+
+                }
+                Console.WriteLine();
+            }
+        }
+        static void ImprimirDiagonal(int[,] Matriz)
+        {
+            Console.WriteLine("Diagonal ai:");
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (j >= i)
+                    {
+                        Console.Write(Matriz[i,j] + "\t ");
+                    }
+                    else { Console.Write("\t "); }
+
+                }
+                Console.WriteLine("\t ");
+            }
+            Console.WriteLine("\t\t ");
+        }
+        static void LeMatriz(int[,] Matriz)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine("[{0},{1}]=", i + 1, j + 1);
+                    Matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
